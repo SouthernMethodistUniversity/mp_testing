6738a7182f24fb19e9a55f051eebad7c15269d19
Robert <rkalescky@smu.edu>
2022-05-21 06:38:40 -0500

    Add hello_world build script.sh

---
+++ 00_hello_world/build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env zsh
+
+# Build using Docker
+docker build -t nvhpc_hello:latest -f ./hello_world.dockerfile .
+
+# Convert from local Docker daemon using enroot
+sudo enroot import -o hello_world.sqsh dockerd://nvhpc_hello:latest
+
+# Run image using enroot
+enroot start hello_world.sqsh
+
